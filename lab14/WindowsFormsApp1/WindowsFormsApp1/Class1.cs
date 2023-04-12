bece019594876cdd925d493da883b97f4042f58e
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:25:19 +0300

    Add files via upload

---
+++ lab14/WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class SortingThread
+    {
+        private Thread sortingThread;
+        private string[] arr;
+        private string algorithm;
+
+        public SortingThread(string[] arr, string algorithm)
+        {
+            this.arr = arr;
+            this.algorithm = algorithm;
+            this.sortingThread = new Thread(new ThreadStart(this.Run));
+        }
+
+        public void Start()
+        {
+            this.sortingThread.Start();
+        }
+
+        public void Join()
+        {
+            this.sortingThread.Join();
+        }
+
+        private void Run()
+        {
+            switch (algorithm)
+            {
+                case "insertion":
+                    InsertionSort(arr);
+                    break;
+                case "selection":
+                    SelectionSort(arr);
+                    break;
+                case "bubble":
+                    BubbleSort(arr);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid algorithm: " + algorithm);
+            }
+        }
+
+        private static void InsertionSort(string[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                string temp = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j].CompareTo(temp) > 0)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+
+        private static void SelectionSort(string[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j].CompareTo(arr[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    string temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+
+        private static void BubbleSort(string[] arr)
+        {
+            bool swapped = true;
+            int j = 0;
+            while (swapped)
+            {
+                swapped = false;
+                j++;
+                for (int i = 0; i < arr.Length - j; i++)
+                {
+                    if (arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        string temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab14/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string[] arr = { "banana", "apple", "cherry", "date", "fig", "elderberry" };
+        private SortingThread insertionThread, selectionThread, bubbleThread;
+        public Form1()
+        {
+            InitializeComponent();
+            insertionThread = new SortingThread(arr, "insertion");
+            selectionThread = new SortingThread(arr, "selection");
+            bubbleThread = new SortingThread(arr, "bubble");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insertionThread.Start();
+            selectionThread.Start();
+            bubbleThread.Start();
+            insertionThread.Join();
+            selectionThread.Join();
+            bubbleThread.Join();
+            label1.Text = string.Join(", ", arr);
+        }
+    }
+}
